12377c5b4abda74a0ac93e95c5b5b8ba98daa3dc
Yaswant Narayan <yoyaswant@gmail.com>
2019-02-11 23:57:50 +0530

    add info activity, user model,

--- app/src/main/java/com/selectmakeathon/app/model/UserModel.java
+++ app/src/main/java/com/selectmakeathon/app/model/UserModel.java
@@ -1,4 +1,22 @@
 package com.selectmakeathon.app.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserModel {
+
+    private String phNo;
+    private String name;
+    private String regNo;
+    private String whatsNo;
+    private boolean isVitian;
+    private boolean isHosteller;
+    private boolean isLeader;
+    private String hostelBlock;
+    private String roomNo;
+    private String email;
+    private String gender;
+    private String branch;
+    private ArrayList<String> skillSet;
+
 }

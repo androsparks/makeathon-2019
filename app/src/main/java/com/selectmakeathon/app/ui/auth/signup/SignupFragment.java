bdf3955b54af94a76f870744413a097635a08eab
Yaswant Narayan <yoyaswant@gmail.com>
2019-02-12 03:46:41 +0530

    add signup layout, update otp fragment

--- app/src/main/java/com/selectmakeathon/app/ui/auth/signup/SignupFragment.java
+++ app/src/main/java/com/selectmakeathon/app/ui/auth/signup/SignupFragment.java
@@ -7,17 +7,41 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.service.quicksettings.Tile;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
+import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.textfield.TextInputLayout;
 import com.selectmakeathon.app.R;
+import com.selectmakeathon.app.model.UserModel;
 import com.selectmakeathon.app.ui.auth.AuthActivity;
 
 public class SignupFragment extends Fragment {
 
+    private TextInputLayout nameInput;
+    private TextInputLayout emailId;
+    private TextInputLayout regNo;
+    private TextInputLayout password;
+    private TextInputLayout confirmPassword;
+    private TextInputLayout whatsappNum;
+    private ChipGroup isVitian;
+    private LinearLayout layoutVitianError;
+    private TextInputLayout collegeNameInput;
+    private ChipGroup isHosteler;
+    private LinearLayout layoutHostelerError;
+    private TextInputLayout blockInput;
+    private TextInputLayout roomNoInput;
+    private ChipGroup gender;
+    private LinearLayout layoutGenderError;
+    private TextInputLayout skillsetInput;
+
     private Button registerButton;
+
+    private UserModel userModel;
 
     public SignupFragment() {
         // Required empty public constructor
@@ -49,6 +73,23 @@ public class SignupFragment extends Fragment {
     }
 
     private void initViews(View view) {
+        nameInput = view.findViewById(R.id.input_name);
+        emailId = view.findViewById(R.id.input_email);
+        regNo = view.findViewById(R.id.input_regno);
+        password = view.findViewById(R.id.input_password);
+        confirmPassword = view.findViewById(R.id.input_password_confirm);
+        whatsappNum = view.findViewById(R.id.input_whatsapp_number);
+        isVitian = view.findViewById(R.id.chip_group_vitian);
+        collegeNameInput = view.findViewById(R.id.input_college_name);
+        isHosteler = view.findViewById(R.id.chip_group_hosteller);
+        blockInput = view.findViewById(R.id.input_hostel_block);
+        roomNoInput = view.findViewById(R.id.input_room_no);
+        gender = view.findViewById(R.id.chip_group_gender);
+        skillsetInput = view.findViewById(R.id.input_skillset);
+        layoutVitianError = view.findViewById(R.id.layout_error_vitian);
+        layoutHostelerError = view.findViewById(R.id.layout_error_hosteler);
+        layoutGenderError = view.findViewById(R.id.layout_error_gender);
+
         registerButton = view.findViewById(R.id.auth_button_register);
     }
 }

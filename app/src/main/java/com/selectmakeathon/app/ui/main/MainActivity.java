168c9c49a85069d2fc48c36f3a9a9364d0fb447b
phantom <shubhamgiri84@gmail.com>
2019-02-12 23:21:30 +0530

    fix home screen timer

--- app/src/main/java/com/selectmakeathon/app/ui/main/MainActivity.java
+++ app/src/main/java/com/selectmakeathon/app/ui/main/MainActivity.java
@@ -2,7 +2,12 @@ package com.selectmakeathon.app.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.TaskStackBuilder;
-
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -24,6 +29,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 import cn.iwgang.countdownview.CountdownView;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,11 +46,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CountdownView countdownView1 = (CountdownView) findViewById(R.id.countdownView);
-        countdownView1.setTag("Lets Begin !");
-        long timer = (long) 5 * 60 * 60 * 1000;
-        countdownView1.start(timer);
+        try {
+            String dateStop = "03/25/2019 18:00:00";
+            Date dtstart = Calendar.getInstance().getTime();
+            DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+            SimpleDateFormat frmt = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+            String dateStart = dateFormat.format(dtstart);
+            Date d1 = null;
+            Date d2 = null;
+            d1 = frmt.parse(dateStart);
+            d2 = frmt.parse(dateStop);
+            long diff = d2.getTime() - d1.getTime();
+            long diffDays = diff / (24 * 60 * 60 * 1000);
 
+
+            CountdownView countdownView1 = (CountdownView) findViewById(R.id.countdownView);
+            countdownView1.setTag("Lets Begin !");
+            long timer = (long) diffDays * 24 * 60 * 60 * 1000;
+            countdownView1.start(timer);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefEditor = prefs.edit();
 

69d9b132254fbaf5060a6f5b014635df4b382c62
reallyinvincible <mailtosparshsri@gmail.com>
2019-02-25 21:57:47 +0530

    Added Model

---
+++ app/src/main/java/com/selectmakeathon/app/ui/team/TeamActivity.java
@@ -0,0 +1,16 @@
+package com.selectmakeathon.app.ui.team;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.selectmakeathon.app.R;
+
+public class TeamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team);
+    }
+}

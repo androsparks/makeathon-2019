fa433830ebc85f0bfcf0db7f2ef0810bda40532f
reallyinvincible <mailtosparshsri@gmail.com>
2019-03-24 19:05:37 +0530

    Added QA adapter

--- app/src/main/java/com/selectmakeathon/app/ui/main/qa/QaAdapter.java
+++ app/src/main/java/com/selectmakeathon/app/ui/main/qa/QaAdapter.java
@@ -1,8 +1,11 @@
 package com.selectmakeathon.app.ui.main.qa;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.selectmakeathon.app.R;
 import com.selectmakeathon.app.model.QaModel;
 
 import java.util.List;
@@ -21,13 +24,16 @@ public class QaAdapter extends RecyclerView.Adapter<QaAdapter.QaViewHolder> {
     @NonNull
     @Override
     public QaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.list_item_qa, parent, false);
+        return new QaViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull QaViewHolder holder, int position) {
-
+        QaModel qaModel = qaModelList.get(position);
+        holder.qaQuestionTextView.setText(qaModel.getQuestion());
+        holder.qaAnswerTextView.setText(qaModel.getAnswer());
     }
 
     @Override
@@ -37,8 +43,12 @@ public class QaAdapter extends RecyclerView.Adapter<QaAdapter.QaViewHolder> {
 
     public class QaViewHolder extends RecyclerView.ViewHolder {
 
+        TextView qaQuestionTextView, qaAnswerTextView;
+
         public QaViewHolder(@NonNull View itemView) {
             super(itemView);
+            qaQuestionTextView = itemView.findViewById(R.id.tv_qa_question);
+            qaAnswerTextView = itemView.findViewById(R.id.tv_qa_answer);
         }
     }
 
